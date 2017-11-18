f37fb82b98b97616f664f6fba9a9f60037b00309
Jackie Baxter <jackeye@gmail.com>
2017-11-18 18:48:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
 // Write your solution here!
-const drivers = ["Milo", "Otis", "Garfield"];
-drivers.length = 0;
-drivers.push (["Milo", "Otis", "Garfield");
-drivers.push('Ralph');
+drivers = ["Milo", "Otis", "Garfield"];
