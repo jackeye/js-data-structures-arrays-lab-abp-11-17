54b377d8dae3f5abcc7d025994a598ce7b299ce2
Jackie Baxter <jackeye@gmail.com>
2017-11-18 19:02:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const drivers = ["Milo", "Otis", "Garfield"];
 drivers.pus();
 
 function destructivelyAppendDriver(name) {
-    const drivers = ["Milo", "Otis", "Garfield"];
+    push.destructivelyAppendDriver("Ralph");
 }
 
-destructivelyAppendDriver(push('Ralph'));
+
